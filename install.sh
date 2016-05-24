0a9c60cbfdbcce47f41dbb4c683780b28b67f609
destruc7i0n <dscdsouza@outlook.com>
2016-05-24 20:49:49 +0000

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,56 @@
+secretKey=""
+rconIP="127.0.0.1"
+rconPort=25575
+rconPass=""
+
+function printSeparator() {
+    echo "------------"
+}
+
+printSeparator
+echo "Thank you for using the BuyCraft Vanilla installer!"
+echo "Before we can complete the installation, we need to ask you some questions"
+echo "These questions will help configure BuyCraft Vanilla so that you can get to using it"
+printSeparator
+
+echo -n "Enter your BuyCraft secret key: "
+read secretKey
+echo -n "Enter the Minecraft server's RCON IP [127.0.0.1]: "
+read temp
+if [ "${temp}" != "" ]; then
+    rconIP=${temp}
+fi
+echo -n "Enter the Minecraft server's RCON port [25575]: "
+read temp
+if [ "${temp}" != "" ]; then
+    rconPort=${temp}
+fi
+echo -n "Enter the Minecraft server's RCON password: "
+stty -echo
+read -s rconPass
+stty echo
+echo
+printSeparator
+
+json=$(printf '{"BUYCRAFT_API_KEY":"%s", "INTERVAL_BETWEEN_COMMAND_SENT": 10, "MINECRAFT_SERVER_RCON_IP": "%s", "MINECRAFT_SERVER_RCON_PORT": %i, "MINECRAFT_SERVER_RCON_PASSWORD": "%s"}\n' "$secretKey" "$rconIP" $rconPort "$rconPass")
+
+PATH=${PWD}/node/bin:$PATH
+if [ ! -d node ]; then
+    echo "Downloading Node..."
+    curl -o node.tar.gz https://nodejs.org/dist/v4.4.4/node-v4.4.4-linux-x64.tar.gz
+    tar -xf node.tar.gz
+    rm -f node.tar.gz
+    mv node-* node
+fi
+
+if [ ! -d "node_modules" ]; then
+    echo "Installing node modules..."
+    sudo npm install
+    sudo npm install forever -g
+fi
+
+echo "Writing config to file..."
+echo ${json} | python -m json.tool > config.json
+echo "Done! Simply start up Vanilla BuyCraft using"
+echo "    ./buycraft start"
+printSeparator
